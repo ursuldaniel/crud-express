cefaa782b7c3e4130561fac47ac063353bb1f923
Daniel Ursul <ursuldm@gmail.com>
2024-06-06 00:49:30 +0300

    readme changes

--- storage.js
+++ storage.js
@@ -3,7 +3,7 @@ const listUsers = require('./routes/listUsers')
 const sqlite3 = require('sqlite3').verbose()
 let sql
 
-const db = new sqlite3.Database('./database.db', sqlite3.OPEN_READWRITE, (err) => {
+const db = new sqlite3.Database('database.db', (err) => {
     if (err) {
         return console.error(err.message)
     }
